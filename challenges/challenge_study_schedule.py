0af61a6666ebbdad70f644d9367e72e7a81ae63a
Lizia Towata <liziatowata@gmail.com>
2023-05-25 14:50:45 -0300

    [feat]: requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    student = 0
+
+    if not target_time or not permanence_period:
+        return None
+
+    for entry, exit in permanence_period:
+        if (type(entry) is not int) or (type(exit) is not int):
+            return None
+
+        if entry <= target_time <= exit:
+            student += 1
+    return student
